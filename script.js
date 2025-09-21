d9380e52a62bb3898c43a00223d8588c4df0349c
jossersolano <jossersol1@gmail.com>
2025-09-21 00:31:08 -0500

    commit avance sitio web

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Happy developing ✨')
+// funcion elaborada con ayuda de copilot
+function includeHTML() {
+  const elemento = document.querySelectorAll('[data-include]');
+  elemento.forEach(el => {
+    const archivoHTML = el.getAttribute('data-include');
+    fetch(archivoHTML)
+      .then(res => res.text())
+      .then(data => el.innerHTML = data);
+  });
+}
+window.onload = includeHTML;
